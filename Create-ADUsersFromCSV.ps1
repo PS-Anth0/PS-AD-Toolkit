f9aa90035ba2d85f7f16328aa53c544227cf962d
Lacrim0sa <anthony.bourgeoisromain@gmail.com>
2024-01-24 11:29:01 +0100

    Create Create-ADUsersFromCSV.ps1

---
+++ Create-ADUsersFromCSV.ps1
@@ -0,0 +1,101 @@
+<#
+.SYNOPSIS
+    Script d'importation en masse d'utilisateur dans un domaine Active Directory
+
+.DESCRIPTION
+
+.AUTHOR
+    BOURGEOIS-ROMAIN Anthony, ingénieur sécurité PAM
+    Contact : anthony.bourgeois-romain@protonmail.com
+
+.VERSION
+    1.0.0
+
+.LINK
+    GitHub : 
+
+.EXAMPLE
+    ./Create-ADUsersFromCSV.ps1 -PasswordLength 20
+
+.NOTES
+    Init du script
+#>
+
+# Params
+param (
+    [Parameter(Mandatory=$true)]
+    [int]$PasswordLength
+)
+
+# Fonctions
+Function Write-Log {
+    param (
+        [string]$message
+    )
+    $timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+    "$timestamp - $message" | Out-File -Append -FilePath $logPath
+}
+
+Function New-Password {
+    param (
+        [int]$length = 10
+    )
+
+    $characters = 'abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNOPQRSTUVWXYZ0123456789!@$?_-'
+    $password = For ($i=0; $i -lt $length; $i++) {
+        $characters[(Get-Random -Maximum $characters.Length)]
+    }
+
+    return [String]::Join('', $password)
+}
+
+# Main Script
+$logFileName = "Log_" + (Get-Date -Format "yyyyMMdd_HHmmss") + ".txt"
+$logPath = "$PSScriptRoot\" + $logFileName
+"--------- Start Job ---------" | Set-Content $logPath
+Write-Log "[INFO] Initialisation du script."
+
+Add-Type -AssemblyName System.Windows.Forms
+
+$openFileDialog = New-Object System.Windows.Forms.OpenFileDialog
+$openFileDialog.InitialDirectory = [Environment]::GetFolderPath("Desktop")
+$openFileDialog.Filter = "CSV files (*.csv)|*.csv"
+$openFileDialog.ShowDialog() | Out-Null
+$csvPath = $openFileDialog.FileName
+
+if (-not (Test-Path $csvPath)) {
+    Write-Host "[WARN] Fichier CSV non sélectionné ou introuvable."
+    Write-Log "[WARN] Fichier CSV non sélectionné ou introuvable."
+    exit
+}
+
+Write-Host "[INFO] Fichier CSV sélectionné : $csvPath"
+Write-Log "[INFO] Fichier CSV sélectionné : $csvPath"
+
+$exportPath = ""
+"login,nom,password" | Set-Content $exportPath
+
+$users = Import-Csv -Path $csvPath
+
+foreach ($user in $users) {
+    $login = $user.matricule
+    $name = $user.nom
+    $passwordText = Generate-Password -length $PasswordLength
+    $password = $passwordText | ConvertTo-SecureString -AsPlainText -Force
+
+    try {
+        Write-Host "[INFO] Création de l'utilisateur : $($user.login)"
+        Write-Log "[INFO] Création de l'utilisateur : $($user.login)"
+        New-ADUser -SamAccountName $login -Name $name -AccountPassword $password -Enabled $true -PasswordNeverExpires $false -ChangePasswordAtLogon $true -Path "OU=Users,DC=example,DC=com" -ErrorAction Stop
+
+        if(Get-ADUser -Filter { SamAccountName -eq $login }) {
+            "$login,$name,$passwordText" | Out-File -Append $exportPath
+        } else {
+            Write-Host "[WARN] L'utilisateur n'a pas pu être vérifié dans l'AD : $($user.login)"
+            Write-Log "[WARN] L'utilisateur n'a pas pu être vérifié dans l'AD : $($user.login)"
+        }
+    } catch {
+        Write-Host "[ERROR] Erreur lors de la création de l'utilisateur : $($user.login)"
+        Write-Log "[ERROR] Erreur lors de la création de l'utilisateur : $($user.login) - $($_.Exception.Message)"
+    }
+}
