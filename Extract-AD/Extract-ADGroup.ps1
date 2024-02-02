01bee4777bb7cc2cc8dff0f26e79c93684660f18
PS-Anth0 <anthony.bourgeoisromain@gmail.com>
2024-02-02 11:55:22 +0100

    NEW Extract Tool

---
+++ Extract-AD/Extract-ADGroup.ps1
@@ -0,0 +1,69 @@
+﻿<#
+.SYNOPSIS
+    Script de scan group AD
+
+.DESCRIPTION
+    Script PowerShell qui va scanner les groupes est utilisateurs présent dans un groupe du domaine
+
+.AUTHOR
+    BOURGEOIS-ROMAIN Anthony, ingénieur sécurité PAM
+    Contact : anthony.bourgeois-romain@protonmail.com
+
+.VERSION
+    1.0.0
+
+.LINK
+    GitHub : https://github.com/PS-Anth0/PS-AD-Toolkit
+
+.EXAMPLE
+    ./Create-ADUsersFromCSV.ps1 -ADPath "OU=Utilisateurs,DC=toto,DC=corp" -PasswordLength 20 -Enabled $true 
+
+.NOTES
+    01/2024 : Init du script
+#>
+
+# Params
+param(
+    [string]$groupFilter
+)
+
+function Get-ADGroupMembersRecursive {
+    param(
+        [string]$GroupName
+    )
+    
+    $groupMembers = Get-ADGroupMember -Identity $GroupName
+    
+    foreach ($member in $groupMembers) {
+        $details = "" | Select-Object GroupName, MemberName, MemberSamAccountName, MemberType
+        $details.GroupName = $GroupName
+        $details.MemberName = $member.Name
+        $details.MemberSamAccountName = $member.SamAccountName
+        $details.MemberType = $member.objectClass
+        
+        $details
+        
+        if ($member.objectClass -eq 'group') {
+            Get-ADGroupMembersRecursive -GroupName $member.Name
+        }
+    }
+}
+
+# Vérifier si le module ActiveDirectory est disponible
+if (-not(Get-Module -ListAvailable -Name ActiveDirectory)) {
+    Write-Error "Le module ActiveDirectory est nécessaire mais n'est pas installé. Veuillez l'installer à l'aide de 'Install-WindowsFeature RSAT-AD-PowerShell' sur un serveur ou télécharger pour un poste client."
+    exit
+}
+
+# Récupérer tous les groupes correspondant au filtre
+$groups = Get-ADGroup -Filter "Name -like '$groupFilter'"
+
+foreach ($group in $groups) {
+    $groupName = $group.Name
+    $groupMembersDetails = Get-ADGroupMembersRecursive -GroupName $groupName
+    
+    # Exporter les informations dans un fichier CSV pour chaque groupe
+    $csvPath = "Group_${groupName}_Members.csv"
+    $groupMembersDetails | Export-Csv -Path $csvPath -NoTypeInformation
+    Write-Host "Les membres du groupe $groupName et de ses sous-groupes ont été exportés dans '$csvPath'."
+}
---
+++ Extract-AD/Extract-ADUser.ps1
@@ -0,0 +1,55 @@
+﻿<#
+.SYNOPSIS
+    Script de scan user AD
+
+.DESCRIPTION
+    Script PowerShell qui va scanner les groupes auxquels appartient un utilisateur du domaine
+
+.AUTHOR
+    BOURGEOIS-ROMAIN Anthony, ingénieur sécurité PAM
+    Contact : anthony.bourgeois-romain@protonmail.com
+
+.VERSION
+    1.0.0
+
+.LINK
+    GitHub : https://github.com/PS-Anth0/PS-AD-Toolkit
+
+.EXAMPLE
+    ./Extract-ADUser.ps1 -userName "Antho"
+
+.NOTES
+    02/02/2024 : Init du script
+#>
+
+# Params
+param(
+    [string]$userName
+)
+
+# Vérifier si le module ActiveDirectory est disponible
+if (-not(Get-Module -ListAvailable -Name ActiveDirectory)) {
+    Write-Error "Le module ActiveDirectory est nécessaire mais n'est pas installé. Veuillez l'installer à l'aide de 'Install-WindowsFeature RSAT-AD-PowerShell' sur un serveur ou télécharger pour un poste client."
+    exit
+}
+
+# Récupérer les groupes dont l'utilisateur est membre
+$userGroups = Get-ADUser -Identity $userName -Properties MemberOf | Select-Object -ExpandProperty MemberOf
+
+# Créer une liste pour stocker les informations des groupes
+$groupInfoList = @()
+
+foreach ($group in $userGroups) {
+    $groupObj = Get-ADGroup -Identity $group -Properties *
+    $groupInfo = New-Object PSObject -Property @{
+        GroupName = $groupObj.Name
+        Description = $groupObj.Description
+        DistinguishedName = $groupObj.DistinguishedName
+    }
+    $groupInfoList += $groupInfo
+}
+
+# Exporter les informations dans un fichier CSV
+$groupInfoList | Export-Csv -Path "User_${userName}_Groups.csv" -NoTypeInformation
+
+Write-Host "Les groupes de l'utilisateur $userName ont été exportés dans 'User_${userName}_Groups.csv'."
